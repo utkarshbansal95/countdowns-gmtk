9e630a1948de5926b6790ef94f8bbdb7eb680b64
utkarshbansal95 <utkarshbansal1995@gmail.com>
2026-07-24 20:21:39 +0530

    Friction added

--- scripts/wrappable.gd
+++ scripts/wrappable.gd
@@ -1,23 +1,16 @@
 class_name Wrappable extends CharacterBody2D
 
 var screen_size: Vector2
-var wrap_margin: float = 50.0
 
 func _ready():
 	screen_size = get_viewport_rect().size
-	for child in get_children():
-		if child is Sprite2D:
-			var size = child.texture.get_size() * child.scale
-			wrap_margin = max(size.x, size.y) / 2.0
-			break
-
-func _physics_process(delta):
-	move_and_slide()
-	if global_position.y+wrap_margin<0:
-		global_position.y=screen_size.y-wrap_margin
-	elif global_position.y+wrap_margin>screen_size.y:
-		global_position.y=-wrap_margin
-	if global_position.x+wrap_margin<0:
-		global_position.x=screen_size.x-wrap_margin
-	elif global_position.x+wrap_margin>screen_size.x:
-		global_position.x=-wrap_margin
+	
+func _process(delta):
+	if global_position.y<0:
+		global_position.y=screen_size.y
+	elif global_position.y>screen_size.y:
+		global_position.y=0
+	if global_position.x<0:
+		global_position.x=screen_size.x
+	elif global_position.x>screen_size.x:
+		global_position.x=0
