6217201e48bab5748f9e86f273fbed9048acf4bd
utkarshbansal95 <utkarshbansal1995@gmail.com>
2026-07-25 21:49:31 +0530

    laser efficiency

---
+++ scripts/game_manager.gd
@@ -0,0 +1,11 @@
+extends Node
+
+@export var player_spawn_position = Vector2(0,0)
+
+var Player: PackedScene
+var Enemy: PackedScene
+
+var rng = RandomNumberGenerator.new()
+
+func _ready():
+	pass
--- scripts/laser.gd
+++ scripts/laser.gd
@@ -4,11 +4,12 @@ extends WrappableArea
 var movement_vector:= Vector2(1,0)
 var displacement_vector: Vector2
 
-
-func _physics_process(delta):
-	displacement_vector=movement_vector*laser_speed*delta
+func _ready() -> void:
+	displacement_vector=movement_vector*laser_speed
 	displacement_vector=displacement_vector.rotated(global_rotation.z)
-	global_position+=Vector3(displacement_vector.x,displacement_vector.y,0)
+	
+func _physics_process(delta):
+	global_position+=Vector3(displacement_vector.x*delta,displacement_vector.y*delta,0)
 
 
 func _on_timer_timeout():
