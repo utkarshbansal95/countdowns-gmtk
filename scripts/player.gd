5b3c0d27376a5573549dbaa947bcbd9aa303389a
Utsav Bansal <utsavbansal93@gmail.com>
2026-07-25 12:16:23 +0530

    3d fied existing project
    Changed Node 2d to 3d, and corresponding changes. Changed sprites to 3d mdels

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,15 @@
 extends WrappableCharacter
 
-@export var acceleration := 12.0
-@export var max_speed := 400.0
+@export var acceleration := 40
+@export var max_speed := 8.0
+@export var damping := 3.0
+
 
 func _physics_process(delta):
-	var input_vector := Vector2(Input.get_axis("move_left","move_right"),Input.get_axis("move_up","move_down"))
-	velocity += input_vector*acceleration
+	var input_vector := Vector3(Input.get_axis("move_left","move_right"),Input.get_axis("move_down","move_up"),0.0) #move up and down swapped as 3d y axis is normal
+	
+	velocity += input_vector*acceleration*delta
 	velocity=velocity.limit_length(max_speed)
-	velocity=velocity.lerp(Vector2(0,0),0.02)
+	velocity=velocity.lerp(Vector3(0,0,0),damping*delta)
+	
 	move_and_slide()
