c963a3330968b535a84c7a55a7b68c78cd0ab7a9
Utsav Bansal <utsavbansal93@gmail.com>
2026-07-25 22:24:33 +0530

    Error Removal from bullet firing
    Changed call order of rotation and position assignment

--- scripts/laser.gd
+++ scripts/laser.gd
@@ -1,15 +1,10 @@
 extends WrappableArea
 
 @export var laser_speed := 5.0
-var movement_vector:= Vector2(1,0)
-var displacement_vector: Vector2
+var direction := Vector3.RIGHT   # set by main after the laser is placed in the world
 
-func _ready() -> void:
-	displacement_vector=movement_vector*laser_speed
-	displacement_vector=displacement_vector.rotated(global_rotation.z)
-	
 func _physics_process(delta):
-	global_position+=Vector3(displacement_vector.x*delta,displacement_vector.y*delta,0)
+	global_position += direction * laser_speed * delta
 
 
 func _on_timer_timeout():
--- scripts/main.gd
+++ scripts/main.gd
@@ -6,9 +6,10 @@ func _ready():
 	pass
 	
 func _on_turret_laser_shot(laser, gp, gr):
-	laser.global_rotation=gr
-	lasers.add_child(laser)
-	laser.global_position=gp + Vector3(0.6,0,0).rotated(Vector3(0,0,1),gr.z)
+	lasers.add_child(laser)                                   # 1) add to the tree FIRST
+	laser.global_rotation = gr                               # 2) now global_* is valid
+	laser.global_position = gp + Vector3(0.6,0,0).rotated(Vector3(0,0,1),gr.z)
+	laser.direction = Vector3(cos(gr.z), sin(gr.z), 0)       # 3) cache heading once, after it's placed
 	
 func _process(_delta):
 	pass
