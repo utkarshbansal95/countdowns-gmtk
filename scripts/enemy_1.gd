47a60444802bd945a917a87220c22fe2a07799c0
Utsav Bansal <utsavbansal93@gmail.com>
2026-07-26 19:03:58 +0530

    Minor Optimization in enemy function

--- scripts/enemy_1.gd
+++ scripts/enemy_1.gd
@@ -3,7 +3,7 @@ extends WrappableCharacter
 @export var speed := 2.0
 @export var enemy_rate_of_fire := 1.0
 @export var enemy_laser_color := Color.RED
-@export var max_health = 3
+@export var max_health = 5
 
 @onready var health = max_health
 @onready var turret: Turret = $turret
@@ -21,21 +21,19 @@ func _physics_process(_delta):
 		var dir := target.global_position - global_position
 		dir.z = 0
 		velocity = dir.normalized() * speed
-	move_and_slide()
-
-func _process(_delta):
-	super._process(_delta)   # keeps WrappableCharacter's screen wrap alive
 	if target:
 		turret.aim_at(target.global_position)
 		turret.try_fire()
+	move_and_slide()
+	
 
 func spawn():
 	pass
 
-func damage(dmg = 1):
+func damage(dmg = 1):   # Animation/sfx for taking damage (Here or maybe on type of damage)
 	health -= dmg
 	if health <= 0:
 		die()
 
-func die():
+func die():   # Animation to be added for death here if I have time
 	queue_free()
